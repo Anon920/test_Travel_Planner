0d27dcf41fc411d983295e76858d88f5ea1c5b09
Dmytro <anonimchik920@gmail.com>
2026-05-29 00:07:22 +0300

    Add endpoints for project places management

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import Base, engine, get_db
-from app.models import TravelProject
-from app.schemas import TravelProjectCreate, TravelProjectRead, TravelProjectUpdate
+from app.external_api import get_word_by_id
+from app.models import TravelProject, ProjectPlace
+from app.schemas import TravelProjectCreate, TravelProjectRead, TravelProjectUpdate, ProjectPlaceRead, \
+    ProjectPlaceCreate
 
 app = FastAPI(title="Travel Planner API")
 
@@ -65,3 +67,132 @@ def delete_project(project_id: int, db: Session = Depends(get_db)):
     db.commit()
 
     return "Project deleted successfully"
+
+@app.post("/projects/{project_id}/places", response_model=ProjectPlaceRead, status_code=201)
+def add_place_to_project(project_id: int, place_data: ProjectPlaceCreate, db: Session = Depends(get_db)):
+
+    project = db.query(TravelProject).filter(TravelProject.id == project_id).first()
+
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+
+    places_count = (db.query(TravelProject).filter(TravelProject.id == project_id).count())
+
+    if places_count >= 10:
+        raise HTTPException(status_code=400, detail="Project cannot have more than 10 places")
+
+    existing_place = (
+        db.query(ProjectPlace)
+        .filter(
+            ProjectPlace.project_id == project_id,
+            ProjectPlace.external_id == place_data.external_id
+        ).first())
+
+    if existing_place:
+        raise HTTPException(status_code=400, detail="This place already exists in the project")
+
+    work = get_word_by_id(place_data.external_id)
+
+    if not work:
+        raise HTTPException(status_code=400, detail="Place not found in external API")
+
+    place = ProjectPlace(
+        project_id=project_id,
+        external_id=work["external_id"],
+        title=work["title"],
+        notes=place_data.notes,
+    )
+
+    db.add(place)
+    db.commit()
+    db.refresh(place)
+
+    return place
+
+@app.get(
+    "/projects/{project_id}/places",
+    response_model=list[ProjectPlaceRead],
+)
+def list_project_places(
+    project_id: int,
+    db: Session = Depends(get_db),
+):
+    project = db.query(TravelProject).filter(TravelProject.id == project_id).first()
+
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+
+    return db.query(ProjectPlace).filter(ProjectPlace.project_id == project_id).all()
+
+
+@app.get(
+    "/projects/{project_id}/places/{place_id}",
+    response_model=ProjectPlaceRead,
+)
+def get_project_place(
+    project_id: int,
+    place_id: int,
+    db: Session = Depends(get_db),
+):
+    place = (
+        db.query(ProjectPlace)
+        .filter(
+            ProjectPlace.id == place_id,
+            ProjectPlace.project_id == project_id,
+        )
+        .first()
+    )
+
+    if not place:
+        raise HTTPException(status_code=404, detail="Place not found")
+
+    return place
+
+
+@app.patch(
+    "/projects/{project_id}/places/{place_id}",
+    response_model=ProjectPlaceRead,
+)
+def update_project_place(
+    project_id: int,
+    place_id: int,
+    place_data: ProjectPlaceUpdate,
+    db: Session = Depends(get_db),
+):
+    place = (
+        db.query(ProjectPlace)
+        .filter(
+            ProjectPlace.id == place_id,
+            ProjectPlace.project_id == project_id,
+        )
+        .first()
+    )
+
+    if not place:
+        raise HTTPException(status_code=404, detail="Place not found")
+
+    update_data = place_data.model_dump(exclude_unset=True)
+
+    for field, value in update_data.items():
+        setattr(place, field, value)
+
+    db.commit()
+    db.refresh(place)
+
+    project_places = (
+        db.query(ProjectPlace)
+        .filter(ProjectPlace.project_id == project_id)
+        .all()
+    )
+
+    project = db.query(TravelProject).filter(TravelProject.id == project_id).first()
+
+    if project_places and all(project_place.visited for project_place in project_places):
+        project.is_completed = True
+    else:
+        project.is_completed = False
+
+    db.commit()
+    db.refresh(place)
+
+    return place
